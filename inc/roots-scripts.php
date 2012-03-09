a569854f97a23120d56d3691c19f3f6e0809ba46
Sam Smith <sam@mintcanary.com>
2012-03-09 16:55:03 +0000

    comments

--- inc/roots-scripts.php
+++ inc/roots-scripts.php
@@ -10,10 +10,13 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_register_script('roots_plugins', '/js/plugins.js', false, null, false);
-wp_register_script('roots_script', '/js/script.js', false, null, false);
-wp_enqueue_script('roots_plugins');
-wp_enqueue_script('roots_script');
+  //wp_register_script('roots_plugins', template_directory_uri() . '/js/plugins.js', false, null, false);
+	//wp_register_script('roots_script', template_directory_uri() . '/js/script.js', false, null, false);
+	wp_register_script('roots_plugins', '/js/plugins.js', false, null, false);
+  wp_register_script('roots_script', '/js/script.js', false, null, false);
+  
+	wp_enqueue_script('roots_plugins');
+  wp_enqueue_script('roots_script');
 }
 
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
