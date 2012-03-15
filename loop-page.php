af2a2074283fb17c3f3ca6af3600f5ad23c26b25
Sam Smith <sam@mintcanary.com>
2012-03-15 10:34:57 +0000

    sidebar fix

--- loop-page.php
+++ loop-page.php
@@ -12,6 +12,7 @@
         <div class="span7">
          <?php the_content(); ?>
         </div>
+      </div>
       
       <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     <?php roots_post_inside_after(); ?>
